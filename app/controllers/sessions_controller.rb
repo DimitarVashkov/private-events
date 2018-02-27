567898b103a429a492fc38743d5a299b1ac40c68
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-02-27 20:35:51 +0000

    Add basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      log_in user
+      flash[:success] = "Logged in!"
+      redirect_to root_url
+    else
+      flash[:danger] = "Bad username!"
+      render 'new'
+    end
+
 
   end
 
   def destroy
+    log_out
     flash[:success] = 'Logged out!'
     redirect_to root_url
   end
