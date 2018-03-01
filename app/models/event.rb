220d6aa9d5ccfaadc3c2bf470cfa0cb33babecc1
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-01 12:05:24 +0000

    Add event scope, show event attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :invitations, foreign_key: :event_id, dependent: :destroy
   has_many :attendees, through: :invitations, source: :attendee
+
+  scope :future, -> { where("date > ?", Time.zone.now) }
+  scope :past, -> { where("date < ?", Time.zone.now) }
 end
